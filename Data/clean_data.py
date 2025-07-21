2d394f75cb7482e3c774df8a1d146b9f4fee06e8
Jucky901 <truongvuon235@gmail.com>
2025-07-21 23:14:26 +0700

    update data

--- Data/clean_data.py
+++ Data/clean_data.py
@@ -5,6 +5,7 @@ df = pd.read_csv('kaggle_datasets_metadata_250721.csv', on_bad_lines='warn', eng
 df['title'] = df['title'].astype(str).str.replace(',', ' ', regex=False)
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 df['lastUpdated'] = pd.to_datetime(df['lastUpdated'], errors='coerce')
+df['lastUpdated'] = df['lastUpdated'].fillna(pd.Timestamp('2025-07-01 00:00:00'))
 df['downloadCount'] = pd.to_numeric(df['downloadCount'], errors='coerce').fillna(0).astype(int)
 df['voteCount'] = pd.to_numeric(df['voteCount'], errors='coerce').fillna(0).astype(int)
 df['usabilityRating'] = pd.to_numeric(df['usabilityRating'], errors='coerce').fillna(0).astype(float)
